c8ea8be21be3d8164b57b44ea68c7e9fd77e9e53
Mateusz Bilicz <mateuszbilicz@gmail.com>
2024-08-13 23:22:56 +0200

    Ngx windows

---
+++ projects/ngx-windows/src/lib/ngx-windows.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-windows',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ngx-windows works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgxWindowsComponent {
+
+}
